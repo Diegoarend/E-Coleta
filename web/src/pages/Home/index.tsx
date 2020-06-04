3eb74896bee3865b0469904061d20be4f43818e5
Diegoarend <diegoaw@gmail.com>
2020-06-04 01:19:53 -0300

    inciando o front-end com react

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css'
+import logo from '../../assets/logo.svg'
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header> 
+                    <img src={logo} alt="ECOLETA"/>
+                </header>
+            </div>
+           
+        </div>
+
+    )
+}
+
+export default Home;
